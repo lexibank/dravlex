d0731727192880be46594e8cb562f12927f7906d
SimonGreenhill <simon@simon.net.nz>
2019-11-28 15:49:56 +0100

    using standard concept ids

--- lexibank_dravlex.py
+++ lexibank_dravlex.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import attr
 from pycldf import Wordlist
+from clldutils.misc import slug
 from pylexibank import Dataset as BaseDataset
 from pylexibank import Cognate, Language
 
@@ -29,7 +30,10 @@ class Dataset(BaseDataset):
         dataset = Wordlist.from_metadata(dsdir / 'Wordlist-metadata.json')
 
         # load concepts
-        args.writer.add_concepts(id_factory=lambda c: c.english)
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id.split('-')[-1]+ '_' + slug(c.english),
+             lookup_factory="Name"
+        )
 
         # load sources from original CLDF, and then the fieldwork source
         args.writer.add_sources(*self.raw_dir.read_bib(dsdir / 'sources.bib'))
@@ -37,7 +41,7 @@ class Dataset(BaseDataset):
         
         # load languages
         self.languages = self.raw_dir.read_csv(dsdir / 'languages.csv', dicts=True)
-        languages = args.writer.add_languages()
+        args.writer.add_languages()
 
         # load cognates
         cogs = {
@@ -51,7 +55,7 @@ class Dataset(BaseDataset):
             for lex in args.writer.add_forms_from_value(
                 Local_ID=row['ID'],
                 Language_ID=row['Language_ID'],
-                Parameter_ID=row['Parameter_ID'],
+                Parameter_ID=concepts[row['Parameter_ID']],
                 Value=row['Form'],
                 Source=src,
                 Comment=row['status'],
